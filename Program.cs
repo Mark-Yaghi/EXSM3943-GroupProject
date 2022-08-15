cadc4300802a835c2289a315b4a0458f669e777f
arvpandit <pandit.arv@gmail.com>
2022-08-15 15:22:49 -0600

    Intitial skaleton;

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+string userChoice = "";
+do
+{
+    Console.WriteLine("1) Enter \"1\" Make Purchase\n2) Enter \"2\" For Admin Login\n3) Enter \"0\" to Quit");
+    Console.Write("Please Make A Selection: ");
+    userChoice = Console.ReadLine().Trim();
+    switch (userChoice)
+    {
+        case "1":
+            Console.WriteLine("Make Purchases Section");
+            break;
+        case "2":
+            Console.WriteLine("Admin Section.");
+            break;
+        case "0":
+            break;
+        default:
+            Console.WriteLine("Invalid input. Please try again !!!");
+            break;
+    }
+} while (userChoice != "0");
+
+
