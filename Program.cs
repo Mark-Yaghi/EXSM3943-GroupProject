978cec0dff66d3faec7efedea67e1eaeed5ad4d0
arvpandit <pandit.arv@gmail.com>
2022-08-15 16:33:12 -0600

    Username and phone validation;

--- Program.cs
+++ Program.cs
@@ -1,5 +1,11 @@
-﻿
+﻿using System.Text.RegularExpressions;
+
 string userChoice = "";
+
+
+
+string userName = "";
+string phoneNumber;
 do
 {
     Console.WriteLine("1) Enter \"1\" Make Purchase\n2) Enter \"2\" For Admin Login\n3) Enter \"0\" to Quit");
@@ -8,7 +14,10 @@ do
     switch (userChoice)
     {
         case "1":
-            Console.WriteLine("Make Purchases Section");
+            Console.WriteLine("Enter your Name: ");
+            userName = Console.ReadLine().Trim();
+            Console.WriteLine("Enter your Phone Number (should be 10 digit long): ");
+            phoneNumber = getValidation("Enter your Phone Number (should be 10 digit long): ", @"^[2-9][\d]{9}$");
             break;
         case "2":
             Console.WriteLine("Admin Section.");
@@ -22,3 +31,28 @@ do
 } while (userChoice != "0");
 
 
+string getValidation(string prompt, string regEx)
+{
+    string output = "";
+    do
+    {
+        Console.Write(prompt);
+        output = Console.ReadLine().ToUpper().Trim();
+    } while (!new Regex(regEx).IsMatch(output));
+    return output;
+}
+
+
+
+
+//public class User
+//{
+//    public User(string userName, int phoneNumber)
+//    {
+//        UserName = userName;
+//        phoneNumber = phoneNumber;
+//    }
+
+//    public string UserName { get; set; }
+//    public int PhoneNumber { get; set; }
+//}
